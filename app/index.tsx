d0d71d33182089dbefafd7a58b77b8558b3512af
Marques Evandro <evandrocastromarques@gmail.com>
2025-05-06 17:13:44 -0400

    ex3-inicio

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,15 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-dark-200 font-bold">Bem vindo</Text>
+    <View>
+      <View className="w-full h-16 bg-purple-600 justify-center items-start pl-4">
+          <Text className="color-slate-50 justify-start text-2xl font-bold">Detalhes do Produto</Text>
+      </View>
+
+      <View className="w-full h-72 justify-center items-center">
+        <Image source={require('../assets/images/copaoAcai.png')} className="max-w-48 max-h-48" resizeMode="cover"></Image>
+      </View>
     </View>
   );
 }
---
+++ atividades/atividade 1/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Image source={require('../assets/images/omago.png')} className="max-w-40 max-h-40 rounded-full" resizeMode="cover" />
+      <Text className="text-4xl text-dark-200 font-bold">Evandro Marques</Text>
+      <Text className="text-2xl text-dark-200 font my-3">Idade: 18</Text>
+      <Text className="text-2xl text-dark-200 font">Localização: Campo Grande</Text>
+      <TouchableOpacity className="bg-blue-500 color-white rounded-xl p-2 px-6 text-center mt-3">Editar perfil</TouchableOpacity>
+    </View> 
+  );
+}
+a
